a33129c832c77a1da3e7375827d4ac909bcbdd4e
gichigi <gichigi@me.com>
2025-11-29 17:15:53 +0000

    Add colorScheme and appleWebApp metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,13 +48,19 @@ export const metadata: Metadata = {
   },
   icons: {
     icon: [
-      { url: "/favicon.ico" },
+      { url: "/favicon.ico", sizes: "any" },
       { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: [
       { url: "/apple-touch-icon.png", sizes: "180x180", type: "image/png" },
     ],
+  },
+  colorScheme: "dark light",
+  appleWebApp: {
+    capable: true,
+    title: "Gravity",
+    statusBarStyle: "default",
   },
   manifest: "/manifest.json",
 };
